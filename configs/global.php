f7d78814d46cf74d6f83355a5e4c4ce55f0fd5e9
GMX9 <41052152+GMX9@users.noreply.github.com>
2018-11-04 13:59:07 +0000

    New config
    You can now configure your API from changenow.io from this config file

--- configs/global.php
+++ configs/global.php
@@ -5,6 +5,7 @@
 
 $config['global']['website'] = "http://exchange.gm-tests.xyz";
 $config['global']['websitename'] = "PTEXCHANGE.";
+$config['changenow']['api'] = "608669356fd610a60e6e146283b923b202986fbc8d60163dcf25cd82b50adc04";
 
 ##################################################
 
