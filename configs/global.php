0211061f3736117119cc422188c1a07ea09a09ef
GMX9 <41052152+GMX9@users.noreply.github.com>
2018-11-04 14:09:47 +0000

    New configurations
    You can now configure the contacts page

--- configs/global.php
+++ configs/global.php
@@ -8,5 +8,13 @@ $config['global']['websitename'] = "PTEXCHANGE.";
 $config['changenow']['api'] = "608669356fd610a60e6e146283b923b202986fbc8d60163dcf25cd82b50adc04";
 
 ##################################################
+# WEBSITE CONTACTS CONFIG
+##################################################
+
+$config['contacts']['location'] = "City, Country HERE"; // Your Company/Website Location
+$config['contacts']['email'] = "City, Country HERE"; // Your Company/Website support email
+$config['contacts']['hours'] = "09:00 - 17:00"; // Support Hours 
+
+##################################################
 
 
