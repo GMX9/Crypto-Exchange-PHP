276562cc08d9efd8efbea97c9d6c837b9950c347
Gonçalo M <41052152+GMX9@users.noreply.github.com>
2018-11-08 16:11:03 +0000

    Update global.php

--- configs/global.php
+++ configs/global.php
@@ -3,7 +3,7 @@
 # GLOBAL  CONFIG
 ##################################################
 
-$config['global']['website'] = "http://exchange.gm-tests.xyz";
+$config['global']['website'] = "http://demo.cryptoexchange-software.xyz";
 $config['global']['websitename'] = "PTEXCHANGE.";
 $config['changenow']['api'] = "608669356fd610a60e6e146283b923b202986fbc8d60163dcf25cd82b50adc04";
 
