a85acd1eaceb45571c5c8b88ea4835e26ed6900f
GMX9 <41052152+GMX9@users.noreply.github.com>
2019-11-11 12:53:00 +0000

    Create smtp.php

---
+++ configs/smtp.php
@@ -0,0 +1,9 @@
+<?php
+##################################################
+# SMTP CONFIGURATION
+##################################################
+$smtp_host = ""; // YOUR SMTP HOST
+$smtp_user = ""; // YOUR SMTP USER
+$smpt_password = ""; // YOUR SMTP PASSWORD
+$smtp_port = "465";  // Ports: 465 or 587
+$smtp_secure = "ssl"; // SSL or TLS
