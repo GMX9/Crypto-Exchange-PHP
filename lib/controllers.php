aa5b797bba8eaa300018aca785e69f596d51cacc
GMX9 <41052152+GMX9@users.noreply.github.com>
2019-11-11 12:54:06 +0000

    Update controllers.php
    Clean of the file  and added new functionalities such as send mail

--- lib/controllers.php
+++ lib/controllers.php
@@ -1,37 +1,32 @@
 <?php 
 require_once("lib/exchange.php");
 
-/*==========================  CRYPTO EXCHANGE SOFTWARE  ============================== */
-/*==========================     Version 1.0     ============================= */
-
 /*
-        Constant: VERSION
-        Contains the framework version. This constant only changes when the framework has a massive update.
-    */
-    const VERSION = '1';
-    
-    /*  
-        Developed by Gonçalo Maia 
-        Constant: REVISION
-        Contains the framework revision. This number changes with each  update of minor changes.
-    */
-    const REVISION = '100';
+====================================================================================================
+-> CRYPTO EXCHANGE SOFTWARE <-
+====================================================================================================
+
+Constant: VERSION
+Contains the framework version. This constant only changes when the framework has a massive update.
+Developed by Gonçalo Maia 
+Constant: REVISION
+Contains the framework revision. This number changes with each  update of minor changes.
+
+====================================================================================================
+*/
+
+const VERSION = '1';
+const REVISION = '100';
 
 class controllers extends exchange{
     
     public $exchange;
     
-public function __construct(){
-    $this->exchange = new exchange;
-}
-
-public function pageIndex($param){
-    $conta = count($_GET);
+    public function __construct(){ $this->exchange = new exchange; }
     
-    if(file_exists("install.php")){
-        include("install.php");
-    }else{
-    
+    public function pageIndex($param){
+        $conta = count($_GET);
+        
         if(isset($_GET['page']) && $conta == 1){
             
                 $name = $_GET[$param];
@@ -57,150 +52,61 @@ public function pageIndex($param){
     	    $module->index();
     
         }
-    }    
-
-}
-
-public function display($path){
     
-    include($path);
-}
-
-public function header_log($title,$css)
-{
-
-    $main = file_get_contents("includes/headerlog-top.html");
-    $st_main = str_replace("[TITLE]",$title,$main);
-    echo $st_main;
-
-    if(!$css == 0){
-
-    echo "<link href='$css' rel='stylesheet' type='text/css'>";
-
+    }
+    
+    // Display file
+    public function display($path){ include($path); }
+    
+    
+    // Display frontend header
+    public function frontend_header($title,$seo){
+       $main = file_get_contents("includes/frontend/header.php");
+       $st_main = str_replace(array('[TITLE]', '[SEO]'), array($title, $seo), $main);
+       echo $st_main;
+    }
+    
+    // Display frontend nav
+    public function frontend_nav(){ include("includes/frontend/navbar.php"); }
+    
+    // Display frontend footer
+    public function frontend_footer(){
+       include("includes/frontend/footer.php");
+    }
+    
+    // Display frontend close header
+    public function frontend_closeHeader(){ include("includes/frontend/close.php"); }
+    
+    //Send emails using PHPMAILER
+    public function sendMail($email,$subject,$corpo){
+        
+        // Configs load
+        require_once("configs/smtp.php");
+        
+        $mail = new PHPMailer\PHPMailer\PHPMailer(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->isSMTP();
+        #$mail->IsMail(); // enable SMTP
+        $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = $smtp_secure; // secure transfer enabled REQUIRED for Gmail
+        $mail->Host = $smtp_host;
+        $mail->Port = $smtp_port; // or 587
+        $mail->IsHTML(true);
+        $mail->Username = $smtp_user;
+        $mail->Password = $smpt_password;
+        $mail->SetFrom($smtp_user);
+        $mail->Subject = $subject;
+        $mail->Body = $corpo;
+                        
+        $mail->AddAddress($email);
+        if(!$mail->Send()) {
+            echo "<script>alert('Erro: $mail->ErrorInfo');</script>";
+        } else {   
+            //echo "<script>alert('Email Sent');</script>";
+        }  
     }
 
-    include('includes/headerlog-close.html');
-
-
-}
-
-
-
-public function header($title){
-
-  #Header New Design
-  global $url;
-  
-  $main = file_get_contents("includes/header.html");
-  $st_main = str_replace(array('[TITLE]', '[WEB]'),array($title, $url),$main);
-
-
-  echo $st_main;
-    //include('includes/header.html');
-}
-
-
-
-public function navbar(){
-
-  # NAVIGATION ALONG WITH SIDEBAR 
-    global $connect;
-    global $_SESSION;
-    global $website_name;
-
-
-
-
-          $main = file_get_contents("includes/navbar.php");
-          $st_main = str_replace("[TITLE]",$website_name,$main);
-          include("includes/navbar.php");
-              
-    
-                
-
-}
-
-
-
-public function footer(){
-
-  include('includes/footer.html');
-
-}
-
-
-
-
-
-public function formOpen($titulo){
-       $main = file_get_contents("resources/backoffice/form.html");
-       $st_main = str_replace("[TITULO]",$titulo,$main);
-       echo $st_main;
-}
-
-public function formClose(){
-       $main = file_get_contents("resources/backoffice/closeform.html");
-       echo $main;
-}
-
-
-public function newInput($label,$name,$value){
-    
-    
-    $main = file_get_contents("resources/backoffice/input.html");
-    $st_main = str_replace(array('[LABEL]', '[NAME]', '[VALUE]'), array($label, $name, $value), $main);
-    echo $st_main;
-    
-}
-
-protected function texto($table,$coluna){
-    
-    global $connect;
-    $select = $connect->query("SELECT * FROM `$table`");
-    $fetch = $select->fetch_array(MYSQLI_ASSOC);
-    $value = $fetch[$coluna];
-    
-    return $value;
-}
-
-public function closeHeader()
-{
-  include('includes/close_header.html');
-}
-
-public function frontend_header($title,$seo){
-   #FRONT-END HEADER
-   $main = file_get_contents("includes/frontend/header.php");
-   $st_main = str_replace(array('[TITLE]', '[SEO]'), array($title, $seo), $main);
-   echo $st_main;
-}
-
-# Mostrar página frontend
-
-public function frontend_nav(){
-   include("includes/frontend/navbar.php");
-}
-
-public function frontend_banner($image){
-   #FRONT-END BANNER
-   if(isset($_SESSION['username'])){
-        $main = file_get_contents("includes/frontend/banner.php");
-
-   }else{
-       
-        $main = file_get_contents("includes/frontend/banner_out.php");
-   }
-   $st_main = str_replace(array('[BANNER]'), array($image), $main);
-   echo $st_main;
-}
-
-public function frontend_footer(){
-   include("includes/frontend/footer.php");
-}
-
-public function frontend_closeHeader(){
-   include("includes/frontend/close.php");
-}
 
 
 
@@ -208,5 +114,8 @@ public function frontend_closeHeader(){
 
 $controllers = new controllers;
 $exchange = new exchange;
+
+
+
 
 ?>
